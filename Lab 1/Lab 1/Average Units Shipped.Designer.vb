e073939f2c31375f9f02638a961244c53471848c
Dakota Parrish <dakota.parrish@dcmail.ca>
2020-06-26 12:02:26 -0400

    Uploaded Lab 1

---
+++ Lab 1/Lab 1/Average Units Shipped.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAvgUnitsShipped
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblAverageUnitsDisplay = New System.Windows.Forms.Label()
+        Me.txtInputDisplay = New System.Windows.Forms.TextBox()
+        Me.lblDays = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.lblUnits = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.AverageUnitsShippedToolTip = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblAverageUnitsDisplay
+        '
+        Me.lblAverageUnitsDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblAverageUnitsDisplay.Location = New System.Drawing.Point(46, 173)
+        Me.lblAverageUnitsDisplay.Name = "lblAverageUnitsDisplay"
+        Me.lblAverageUnitsDisplay.Size = New System.Drawing.Size(177, 23)
+        Me.lblAverageUnitsDisplay.TabIndex = 4
+        Me.lblAverageUnitsDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.lblAverageUnitsDisplay, "Displays the average units shipped per day once all 7 days have been entered")
+        '
+        'txtInputDisplay
+        '
+        Me.txtInputDisplay.Location = New System.Drawing.Point(46, 49)
+        Me.txtInputDisplay.Multiline = True
+        Me.txtInputDisplay.Name = "txtInputDisplay"
+        Me.txtInputDisplay.ReadOnly = True
+        Me.txtInputDisplay.Size = New System.Drawing.Size(174, 121)
+        Me.txtInputDisplay.TabIndex = 3
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.txtInputDisplay, "Displays the units shipped for each day.")
+        '
+        'lblDays
+        '
+        Me.lblDays.Location = New System.Drawing.Point(165, 21)
+        Me.lblDays.Name = "lblDays"
+        Me.lblDays.Size = New System.Drawing.Size(80, 23)
+        Me.lblDays.TabIndex = 2
+        Me.lblDays.Text = "Day 1"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(103, 22)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(56, 20)
+        Me.txtInput.TabIndex = 1
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.txtInput, "Type the units shipped for each day")
+        '
+        'lblUnits
+        '
+        Me.lblUnits.Location = New System.Drawing.Point(43, 21)
+        Me.lblUnits.Name = "lblUnits"
+        Me.lblUnits.Size = New System.Drawing.Size(64, 21)
+        Me.lblUnits.TabIndex = 0
+        Me.lblUnits.Text = "Units:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(176, 213)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(69, 28)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "E&xit"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnExit, "Closes the program")
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(89, 213)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(80, 28)
+        Me.btnReset.TabIndex = 6
+        Me.btnReset.Text = "&Reset"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnReset, "Clears all text boxes and the output label's text.")
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(12, 213)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(71, 28)
+        Me.btnEnter.TabIndex = 5
+        Me.btnEnter.Text = "&Enter"
+        Me.AverageUnitsShippedToolTip.SetToolTip(Me.btnEnter, "Enters the units shipped into the ")
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'frmAvgUnitsShipped
+        '
+        Me.AcceptButton = Me.btnEnter
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(294, 261)
+        Me.Controls.Add(Me.lblAverageUnitsDisplay)
+        Me.Controls.Add(Me.txtInputDisplay)
+        Me.Controls.Add(Me.lblDays)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.lblUnits)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnEnter)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(310, 300)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(310, 300)
+        Me.Name = "frmAvgUnitsShipped"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Average Units Shipped"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblAverageUnitsDisplay As Label
+    Friend WithEvents txtInputDisplay As TextBox
+    Friend WithEvents lblDays As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents lblUnits As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnEnter As Button
+    Friend WithEvents AverageUnitsShippedToolTip As ToolTip
+End Class
---
+++ Lab 1/Lab 1/Average Units Shipped.vb
@@ -0,0 +1,98 @@
+﻿'Name: Dakota Parrish
+'Student Number: 100764512
+'Program Name: Average Units Shipped
+'Date: May 28, 2020
+'Description: This program allows the entry of a unit number for each day numbered 1 to 7 and adds it to a running total. Once the days reaches to 7 days, 
+'it will Then take the total and divides it by 7 to find out the average units shipper per day and displays it to the user. You can exit the program any time
+'by hitting the exit button and you can also reset the program by hitting the reset button. 
+
+Option Strict On
+
+Public Class frmAvgUnitsShipped
+    Dim days(6) As Integer 'Declare days as an array with 6 elements.
+    Dim currentDayUnits As Integer = 0 'Declare the current day units being shipped as an integer and set it as 0. This will be the element used in 'days(6).
+    Dim avgTotal As Double 'Declare the average total as a double.
+    Dim runningTotal As Integer 'Declare the running total as an integer.
+
+    'Whenever the user clicks on the enter button, it will perform two purposes. It will 1. Add their number for 'units' and
+    'add it to a running total after it has been validated, and 2 once the 'Days' hits 7, it will then take the running total and divide it by 7
+    'to display the average total.
+    Private Sub btnEnter_Click(sender As Object, e As EventArgs) Handles btnEnter.Click
+
+        'If the current day is less than 7.
+        If currentDayUnits < 7 Then
+            'First, checks if the units entered number is a whole number.
+            If Integer.TryParse(txtInput.Text, days(currentDayUnits)) Then
+
+                'Checks to see if number entered is within the range of 0-5000.
+                If days(currentDayUnits) >= 0 And days(currentDayUnits) <= 5000 Then
+
+                    'The unit number entered is within the range.
+                    'The number entered for units is appended to the input display textbox.
+                    txtInputDisplay.Text &= days(currentDayUnits) & vbCrLf
+                    'Clears the text box for input.
+                    txtInput.Clear()
+                    'Sets focus on the input text box so the user can type in another number.
+                    txtInput.Focus()
+                    'Adds the units entered to the running total.
+                    runningTotal += days(currentDayUnits)
+                    'Since the element used in the 'days(6)' array is 'currentDayUnits' we use it for the day counter and add one to it.
+                    currentDayUnits = currentDayUnits + 1
+                    'Changes the label to display and keep track of the day number for the user as they are entering units.
+                    lblDays.Text = "Day " & (currentDayUnits + 1)
+
+                    'If the day number is equal to 7.
+                    If currentDayUnits = 7 Then
+                        'Set the text for the days label to say 'Day 7'
+                        lblDays.Text = "Day 7"
+                        'Sets the ReadOnly property of the input textbox to true so the user can not enter any more numbers.
+                        txtInput.ReadOnly = True
+                        'Sets the Enabled property of the enter button to false so the user can't hit the enter button anymore.
+                        btnEnter.Enabled = False
+                        'Calculates the average total by taking the running total and dividing it by 7.
+                        avgTotal = runningTotal / 7
+                        'Displays the average units per day and rounds it to 2 decimal places.
+                        lblAverageUnitsDisplay.Text = "Average per day:" & CStr(Math.Round(avgTotal, 2))
+                    End If
+                Else
+                    'The unit numbered entered is NOT within the range.
+                    MessageBox.Show("The number entered must be between the range of 0-5000!")
+                    'Focuses the input text box for re-entry.
+                    txtInput.Focus()
+                End If
+            Else
+                'The number entered was not a whole number!
+                MessageBox.Show("The number entered must be a whole number.")
+                'Focuses the input text box for re-entry.
+                txtInput.Focus()
+            End If
+        End If
+    End Sub
+    'Whenever the user clicks on the reset button, it essentially resets the form and clears all text boxes and labels as well as sets the variables
+    'and totals back to 0 to match the state of the form as it was once opened.
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        'Clears the input display textbox.
+        txtInputDisplay.Clear()
+        'Clears the input text box.
+        txtInput.Clear()
+        'Clears the total average label.
+        lblAverageUnitsDisplay.Text = ""
+        'Re-enables the enter button.
+        btnEnter.Enabled = True
+        'Sets the label for days to say Day 1.
+        lblDays.Text = "Day 1"
+        'Re-enables the input text box.
+        txtInput.ReadOnly = False
+        'Clears the currentDayUnits value and sets it back to 0.
+        currentDayUnits = 0
+        'Clears the running total value and sets it back to 0.
+        runningTotal = 0
+        'Sets the focus on the input text box for re-entry.
+        txtInput.Focus()
+    End Sub
+    '''Whenever the user clicks on the exit button, it will gracefully close the form.
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Closes the form.
+        Application.Exit()
+    End Sub
+End Class
